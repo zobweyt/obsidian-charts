e093ef0a3c1ace52f3d3c727a65f04cce9175046
Nikita Gaziev <zobweyt@gmail.com>
2026-05-23 19:30:19 +0000

    Remove dynamic code execution

--- scripts/build.ts
+++ scripts/build.ts
@@ -6,6 +6,38 @@ const outdir = isWatch
   ? "./example/.obsidian/plugins/bases-chart-layouts"
   : "./dist";
 
+const cleanEchartsPlugin: esbuild.Plugin = {
+  name: "clean-echarts-and-obsidian-compliance",
+  setup(build) {
+    build.onLoad(
+      { filter: /node_modules\/(echarts|zrender)\/.*\.js$/ },
+      async (args) => {
+        let contents = await Deno.readTextFile(args.path);
+
+        contents = contents.replace(
+          /\bnew\s+Function\b/g,
+          "(() => { throw new Error('Dynamic functions are blocked'); })",
+        );
+        contents = contents.replace(
+          /\bFunction\((['"])[^'"]+\1\)/g,
+          "(() => { throw new Error('Dynamic code execution is blocked'); })",
+        );
+
+        contents = contents.replace(
+          /\batob\(([^)]+)\)/g,
+          "Buffer.from($1, 'base64').toString('binary')",
+        );
+        contents = contents.replace(
+          /\bbtoa\(([^)]+)\)/g,
+          "Buffer.from($1, 'binary').toString('base64')",
+        );
+
+        return { contents, loader: "js" };
+      },
+    );
+  },
+};
+
 const config: esbuild.BuildOptions = {
   entryPoints: ["./src/main.ts", "./src/styles.css"],
   outdir,
@@ -18,6 +50,11 @@ const config: esbuild.BuildOptions = {
   platform: "node",
   external: ["obsidian"],
   logLevel: "info",
+  plugins: [cleanEchartsPlugin],
+  define: {
+    "eval": "undefined",
+    "globalThis.eval": "undefined",
+  },
 };
 
 if (isWatch) {
